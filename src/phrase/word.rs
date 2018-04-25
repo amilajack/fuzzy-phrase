53dcf39cdaeb3a4feecf062b57186f775f0d0d1e
Bob Lannon <bob.lannon@mapbox.com>
2018-04-24 18:43:02 -0700

    enum for word

--- src/phrase/word.rs
+++ src/phrase/word.rs
@@ -1,11 +1,22 @@
-struct Word {
-    word: String,
-    id: u64,
-    edit_distance: u64,
+enum Word {
+    Full {
+        word: String,
+        id: u64,
+        edit_distance: u64,
+    },
+    Prefix {
+        word: String,
+        id_range: (u64, u64),
+        edit_distance: u64,
+    },
 }
 
-struct WordPrefix {
-    word: String,
-    id_range: (u64, u64),
-    edit_distance: u64,
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn full_and_prefix_logic() {
+        // TODO: write test <24-04-18, boblannon> //
+    }
 }
