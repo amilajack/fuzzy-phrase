874c85f281861baa5dc417e3076a5b95ba8edb10
Bob Lannon <bob.lannon@mapbox.com>
2018-04-30 13:31:10 -0400

    update prefix and add test

--- src/phrase/word.rs
+++ src/phrase/word.rs
@@ -1,13 +1,16 @@
-enum Word {
+use std::ops::Range;
+
+pub enum Word {
     Full {
-        word: String,
+        string: String,
         id: u64,
         edit_distance: u64,
     },
     Prefix {
-        word: String,
-        id_range: (u64, u64),
-        edit_distance: u64,
+        string: String,
+        id_range: Range<u64>,
+        // not sure this makes sense
+        // edit_distance: u64,
     },
 }
 
@@ -16,7 +19,45 @@ mod tests {
     use super::*;
 
     #[test]
-    fn full_and_prefix_logic() {
-        // TODO: write test <24-04-18, boblannon> //
+    fn two_exact_matches_one_prefix() {
+        let phrase: Vec<Word> = vec![
+            // two words
+            Word::Full{ string: String::from("100") , id: 1u64,      edit_distance: 0 },
+            Word::Full{ string: String::from("main"), id: 61_528u64, edit_distance: 0 },
+            // last word is a prefix
+            Word::Prefix{
+                string: String::from("st"),
+                id_range: Range { start: 561_528u64, end: 561_531u64 }
+            },
+        ];
+
+        let mut word_count = 0;
+        let mut word_ids = vec![];
+
+        let mut prefix_count = 0;
+        let mut prefix_ids = vec![];
+
+        for word in phrase.into_iter() {
+            match word {
+                Word::Full{ string, id, edit_distance } => {
+                    word_count += 1;
+                    word_ids.push(id);
+                },
+                Word::Prefix{ string, id_range } => {
+                    prefix_count += 1;
+                    for i in id_range {
+                        prefix_ids.push(i);
+                    }
+                }
+            }
+        }
+
+        // should be 2 full words, 2 ids
+        assert_eq!(2, word_count);
+        assert_eq!(vec![1, 61_528], word_ids);
+
+        // should be 1 prefix, 3 ids
+        assert_eq!(1, prefix_count);
+        assert_eq!(vec![561_528, 561_529, 561_530], prefix_ids);
     }
 }
