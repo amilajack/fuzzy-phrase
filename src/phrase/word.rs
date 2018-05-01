ab08a1feb32d8a29a9e2070570c50617f3d6888c
Bob Lannon <bob.lannon@mapbox.com>
2018-04-30 20:09:55 -0400

    edit distance back to Word and use refs instead of moving

--- src/phrase/word.rs
+++ src/phrase/word.rs
@@ -3,6 +3,7 @@ pub enum Word {
     Full {
         string: String,
         id: u64,
+        edit_distance: i8,
     },
     Prefix {
         string: String,
@@ -14,16 +15,14 @@ pub enum Word {
 pub struct Phrase<'a> {
     length: usize,
     words: &'a[&'a Word],
-    edit_distances: &'a[i32],
 }
 
 impl<'a> Phrase<'a> {
-    pub fn new(words: &'a[&'a Word], edit_distances: &'a [i32]) -> Phrase<'a> {
+    pub fn new(words: &'a[&'a Word]) -> Phrase<'a> {
         let length: usize = words.len();
         Phrase {
             words,
             length,
-            edit_distances,
         }
     }
 
@@ -36,7 +35,6 @@ impl<'a> Phrase<'a> {
 pub struct PhraseIterator<'a> {
     phrase: &'a Phrase<'a>,
     offset: usize,
-    total_distance: i32,
 }
 
 impl<'a> Iterator for PhraseIterator<'a> {
@@ -46,8 +44,7 @@ impl<'a> Iterator for PhraseIterator<'a> {
         if self.offset >= self.phrase.length {
             return None
         } else {
-            let word = self.phrase.words[self.offset];
-            self.total_distance += self.phrase.edit_distances[self.offset];
+            let ref word = self.phrase.words[self.offset];
             self.offset += 1;
             Some(word)
         }
@@ -59,7 +56,7 @@ impl<'a> IntoIterator for &'a Phrase<'a> {
     type IntoIter = PhraseIterator<'a>;
 
     fn into_iter(self) -> Self::IntoIter {
-        PhraseIterator{ phrase: self, offset: 0, total_distance: 0}
+        PhraseIterator{ phrase: self, offset: 0 }
     }
 }
 
@@ -71,23 +68,19 @@ mod tests {
     fn two_exact_matches_one_prefix() {
 
         // two words
-        let word_one = Word::Full{ string: String::from("100"), id: 1u64 };
-        let word_two = Word::Full{ string: String::from("main"), id: 61_528u64 };
+        let word_one = Word::Full{ string: String::from("100"), id: 1u64, edit_distance: 0 };
+        let word_two = Word::Full{ string: String::from("main"), id: 61_528u64, edit_distance: 0 };
         // last word is a prefix
         let word_three = Word::Prefix{
             string: String::from("st"),
             id_range: (561_528u64, 561_531u64),
         };
 
-        let word_seq = [
-            &word_one,
-            &word_two,
-            &word_three
-        ];
+        let word_seq = [ &word_one, &word_two, &word_three ];
 
-        let edit_distances = [ 0, 0, 0 ];
-        let phrase = Phrase::new(&word_seq, &edit_distances);
+        let phrase = Phrase::new(&word_seq);
 
+        let mut total_distance = 0;
         let mut word_count = 0;
         let mut word_ids = vec![];
 
@@ -96,13 +89,14 @@ mod tests {
 
         let phrase_iter = phrase.into_iter();
 
-        for ref word in phrase_iter {
+        for word in phrase_iter {
             match word {
-                &&Word::Full{ ref string,  ref id } => {
+                &Word::Full{ ref string,  ref id, ref edit_distance } => {
                     word_count += 1;
+                    total_distance += edit_distance;
                     word_ids.push(*id);
                 },
-                &&Word::Prefix{ ref string, ref id_range } => {
+                &Word::Prefix{ ref string, ref id_range } => {
                     prefix_count += 1;
                     for i in (*id_range).0..(*id_range).1 {
                         prefix_ids.push(i);
@@ -110,7 +104,7 @@ mod tests {
                 }
             }
         }
-        assert_eq!(0, phrase_iter.total_distance);
+        assert_eq!(0, total_distance);
 
         // should be 2 full words, 2 ids
         assert_eq!(2, word_count);
@@ -125,27 +119,24 @@ mod tests {
     fn two_fuzzy_matches() {
 
         // two words
-        let word_one = Word::Full{ string: String::from("100"), id: 1u64 };
-        let word_two = Word::Full{ string: String::from("main"), id: 61_528u64 };
+        let word_one = Word::Full{ string: String::from("100"), id: 1u64, edit_distance: 1 };
+        let word_two = Word::Full{ string: String::from("main"), id: 61_528u64, edit_distance: 2 };
 
-        let word_seq = [
-            &word_one,
-            &word_two,
-        ];
+        let word_seq = [ &word_one, &word_two ];
 
-        let edit_distances = [ 1, 2 ];
-        let phrase = Phrase::new(&word_seq, &edit_distances);
+        let phrase = Phrase::new(&word_seq);
 
         let mut word_count = 0;
         let mut word_ids = vec![];
+        let mut total_distance = 0;
 
         let phrase_iter = phrase.into_iter();
 
-        for (i, ref word) in phrase_iter.enumerate() {
-            assert_eq!(edit_distances[i], phrase_iter.total_distance);
+        for word in phrase_iter {
             match word {
-                &&Word::Full{ ref string,  ref id } => {
+                &Word::Full{ ref string,  ref id, ref edit_distance } => {
                     word_count += 1;
+                    total_distance += edit_distance;
                     word_ids.push(*id);
                 },
                 _ => {
@@ -153,11 +144,64 @@ mod tests {
                 }
             }
         }
-        assert_eq!(3, phrase_iter.total_distance);
+        assert_eq!(3, total_distance);
 
         // should be 2 full words, 2 ids
         assert_eq!(2, word_count);
         assert_eq!(vec![1, 61_528], word_ids);
 
+    }
+
+    #[test]
+    fn multiple_combinations() {
+        // three words, two variants for third word
+        let word_one = Word::Full{ string: String::from("Evergreen"), id: 1u64, edit_distance: 0 };
+        let word_two = Word::Full{ string: String::from("Terrace"), id: 61_528u64, edit_distance: 0 };
+        let word_three_a = Word::Full{ string: String::from("Springfield"), id: 561_235u64, edit_distance: 0 };
+        let word_three_b = Word::Full{ string: String::from("Sprungfeld"), id: 561_247u64, edit_distance: 2 };
+
+        let words_a = [ &word_one, &word_two, &word_three_a ];
+        let phrase_a = Phrase::new(&words_a);
+
+        let mut word_ids = vec![];
+        let mut total_distance = 0;
+
+        for word in phrase_a.into_iter() {
+            match word {
+                &Word::Full{ ref string,  ref id, ref edit_distance } => {
+                    word_ids.push(*id);
+                    total_distance += edit_distance;
+                },
+                _ => {
+                    panic!("Should be all full words");
+                }
+            }
+        }
+        assert_eq!(0, total_distance);
+
+        // should be 2 full words, 2 ids
+        assert_eq!(vec![1u64, 61_528u64, 561_235u64], word_ids);
+
+        let words_b = [ &word_one, &word_two, &word_three_b ];
+        let phrase_b = Phrase::new(&words_b);
+
+        let mut word_ids = vec![];
+        let mut total_distance = 0;
+
+        for word in phrase_b.into_iter() {
+            match word {
+                &Word::Full{ ref string,  ref id, ref edit_distance } => {
+                    word_ids.push(*id);
+                    total_distance += edit_distance;
+                },
+                _ => {
+                    panic!("Should be all full words");
+                }
+            }
+        }
+        assert_eq!(2, total_distance);
+
+        // should be 2 full words, 2 ids
+        assert_eq!(vec![1u64, 61_528u64, 561_247u64], word_ids);
     }
 }
