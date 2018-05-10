3ab2021a22c5cdb71e8f5b8c21e296a00c0c7970
Andrew Pendleton <andrew@mapbox.com>
2018-05-10 19:20:01 -0400

    Flesh out a little more of the builder finish operation

--- src/glue/mod.rs
+++ src/glue/mod.rs
@@ -1,16 +1,19 @@
 use std::collections::BTreeMap;
 use std::path::{Path, PathBuf};
 use std::error::Error;
-use std::io::{Error as IoError, ErrorKind as IoErrorKind};
+use std::io::{Error as IoError, ErrorKind as IoErrorKind, BufWriter};
 use std::fs;
+
+use ::prefix::PrefixSetBuilder;
+use ::phrase::PhraseSetBuilder;
 
 #[derive(Default, Debug)]
 pub struct FuzzyPhraseSetBuilder {
-    phrases: Vec<Vec<u32>>,
+    phrases: Vec<Vec<u64>>,
     // use a btreemap for this one so we can read them out in order later
     // we'll only have one copy of each word, in the vector, so the inverse
     // map will map from a pointer to an int
-    words_to_tmpids: BTreeMap<String, u32>,
+    words_to_tmpids: BTreeMap<String, u64>,
     directory: PathBuf,
 }
 
@@ -42,13 +45,13 @@ impl FuzzyPhraseSetBuilder {
     }
 
     pub fn insert(&mut self, phrase: &Vec<&str>) -> Result<(), Box<Error>> {
-        let mut tmpid_phrase: Vec<u32> = Vec::with_capacity(phrase.len());
+        let mut tmpid_phrase: Vec<u64> = Vec::with_capacity(phrase.len());
         for word in phrase {
             // the fact that this allocation is necessary even if the string is already in the hashmap is a bummer
             // but absent https://github.com/rust-lang/rfcs/pull/1769 , avoiding it requires a huge amount of hoop-jumping
             let string_word = word.to_string();
             let current_len = self.words_to_tmpids.len();
-            let word_id = self.words_to_tmpids.entry(string_word).or_insert(current_len as u32);
+            let word_id = self.words_to_tmpids.entry(string_word).or_insert(current_len as u64);
             tmpid_phrase.push(word_id.to_owned());
         }
         self.phrases.push(tmpid_phrase);
@@ -59,11 +62,26 @@ impl FuzzyPhraseSetBuilder {
         // we can go from name -> tmpid
         // we need to go from tmpid -> id
         // so build a mapping that does that
-        let mut tmpids_to_ids: Vec<u32> = vec![0; self.words_to_tmpids.len()];
+        let mut tmpids_to_ids: Vec<u64> = vec![0; self.words_to_tmpids.len()];
 
-        for (id, (_key, tmpid)) in self.words_to_tmpids.iter().enumerate() {
-            tmpids_to_ids[*tmpid as usize] = id as u32;
+        let prefix_writer = BufWriter::new(fs::File::create(self.directory.join(Path::new("prefix.fst")))?);
+        let mut prefix_set_builder = PrefixSetBuilder::new(prefix_writer)?;
+
+        // words_to_tmpids is a btreemap over word keys,
+        // so when we iterate over it, we'll get back words sorted
+        // we'll do three things with that:
+        // - build up our prefix set
+        // - map from temporary IDs to lex ids (which we can get just be enumerating our sorted list)
+        // - build up our fuzzy set (this one doesn't require the sorted words, but it doesn't hurt)
+        for (id, (word, tmpid)) in self.words_to_tmpids.iter().enumerate() {
+            prefix_set_builder.insert(word)?;
+
+            tmpids_to_ids[*tmpid as usize] = id as u64;
+
+            // TODO: insert into fuzzy map
         }
+
+        prefix_set_builder.finish()?;
 
         // next, renumber all of the current phrases with real numbers
         for phrase in self.phrases.iter_mut() {
@@ -73,6 +91,15 @@ impl FuzzyPhraseSetBuilder {
         }
 
         self.phrases.sort();
+
+        let phrase_writer = BufWriter::new(fs::File::create(self.directory.join(Path::new("phrase.fst")))?);
+        let mut phrase_set_builder = PhraseSetBuilder::new(phrase_writer)?;
+
+        for phrase in self.phrases {
+            phrase_set_builder.insert(&phrase)?;
+        }
+
+        phrase_set_builder.finish()?;
 
         Ok(())
     }
