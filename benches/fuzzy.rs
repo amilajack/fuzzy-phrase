799efdb53776c0b734593acd6dcec22125eff396
aarthykc <aarthy@mapbox.com>
2018-06-07 15:41:14 +0530

    working version

--- benches/fuzzy.rs
+++ benches/fuzzy.rs
@@ -2,35 +2,49 @@ use criterion::{Criterion, Fun, Bencher};
 use reqwest;
 use fuzzy_phrase::fuzzy::map::FuzzyMap;
 use fuzzy_phrase::fuzzy::map::FuzzyMapBuilder;
-use fst::raw::Output;
 use std::rc::Rc;
-use itertools::Itertools;
 
 pub fn benchmark(c: &mut Criterion) {
     extern crate tempfile;
+
+    //share a list of words across benchmarks
+    //and a built fuzzy map, so define a struct to contain them
     struct BenchData {
         words: Vec<String>,
-        fuzzy_map: FuzzyMap
+        fuzzymap: FuzzyMap
     };
 
     // fetch data and build the structures
     let wordlist = reqwest::get("https://raw.githubusercontent.com/BurntSushi/fst/master/data/words-10000")
             .expect("tried to download data")
             .text().expect("tried to decode the data");
+
     let words = wordlist.trim().split("\n").map(|w| w.to_owned()).collect::<Vec<String>>();
 
+    //build_to_iter expects a path to build the structure
     let dir = tempfile::tempdir().unwrap();
     let file_start = dir.path().join("fuzzy");
-    FuzzyMapBuilder::build_from_iter(&file_start, words.iter().cloned(), 2).unwrap();
+
+    // build the structure
+    FuzzyMapBuilder::build_from_iter(&file_start, words.iter().map(|s| s.as_ref()) , 1);
     let map = unsafe { FuzzyMap::from_path(&file_start).unwrap() };
-    let shared_data = Rc::new(BenchData { words, map });
+
+    // move the prebuilt data into a reference-counted struct
+    let shared_data = Rc::new(BenchData { words: words, fuzzymap: map });
+    // make a vector to fill with closures to bench-test
     let mut to_bench = Vec::new();
 
-    let data = shared_data.clone();
+    to_bench.push(Fun::new("exact_match", move |b: &mut Bencher, _i| {
+        // we're benching on a list of words, but criterion needs to run for as long as it wants
+        // to get a statistically significant sample, potentially for more iterations than we have
+        // words, so we'll build all the benches around cycle iterators that go forever
+        let mut cycle = shared_data.words.iter().cycle();
 
-    to_bench.push(Fun::new("exact_contains", move |b: &mut Bencher, _i| {
-        let mut cycle = data.words.iter().cycle();
-        b.iter(|| data.map.lookup(cycle.next().unwrap()));
+        //this is the part that is timed
+        b.iter(|| {
+            let _matches = shared_data.fuzzymap.lookup(cycle.next().unwrap(), 1, |id| &shared_data.words[id as usize]);
+        });
     }));
-    c.bench_functions("prefix", to_bench, ());
+    // run benchmarks
+    c.bench_functions("fuzzy", to_bench, ());
 }
