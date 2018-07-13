cadbfeea0b98c6afba2af458664b9b599c65f7ba
aarthykc <aarthy@mapbox.com>
2018-07-13 20:48:36 +0530

    checking whether fuzzy multi window and windowed match give the same result

--- src/glue/fuzz_tests.rs
+++ src/glue/fuzz_tests.rs
@@ -71,3 +71,45 @@ fn glue_fuzztest_match_prefix() {
         }
     }
 }
+
+#[test]
+#[ignore]
+fn fuzzy_match_windowed_multi_equivalent_test() {
+    let cities: Vec<&str> = include_str!("../../benches/data/phrase_test_cities.txt").trim().split("\n").collect();
+    let states: Vec<&str> = include_str!("../../benches/data/phrase_test_states.txt").trim().split("\n").collect();
+    let mut rng = rand::thread_rng();
+    let mut augmented_phrases: Vec<String> = Vec::with_capacity(1000);
+    for _i in 0..1000 {
+        let phrase = rng.choose(&PHRASES).unwrap();
+        let damaged = get_damaged_phrase(phrase, |w| SET.can_fuzzy_match(w));
+        let zip: u32 = rng.gen_range(10000, 99999);
+        let augmented = format!(
+            "{addr} {city} {state} {zip}",
+            addr = damaged,
+            city = rng.choose(&cities).unwrap(),
+            state = rng.choose(&states).unwrap(),
+            zip = zip
+        );
+        augmented_phrases.push(augmented);
+    }
+
+    for phrase in augmented_phrases.iter() {
+        let tokens: Vec<_> = phrase.split(" ").collect();
+        let mut variants: Vec<(Vec<&str>, bool)> = Vec::new();
+        let windowed_match_result = SET.fuzzy_match_windows(tokens.as_slice(), 1, 1, false).unwrap();
+        for start in 0..tokens.len() {
+            for end in start..tokens.len() {
+                variants.push((tokens[start..(end + 1)].to_vec(), false));
+            }
+        }
+        let windowed_match_multi_result = SET.fuzzy_match_multi(variants.as_slice(), 1, 1).unwrap();
+        //check if windowed match and multi windowed match give the same results
+        assert!(windowed_match_result.iter().any(|x| {
+            windowed_match_multi_result.iter().any(|y| {
+                y.iter().any(|z| {
+                    x == z
+                })
+            })
+        }));
+    }
+}
